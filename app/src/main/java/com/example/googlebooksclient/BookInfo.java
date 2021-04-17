072fe484ed0c440df3a37757624787ed0a913131
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-04-17 12:41:15 +0200

    update

--- app/src/main/java/com/example/googlebooksclient/BookInfo.java
+++ app/src/main/java/com/example/googlebooksclient/BookInfo.java
@@ -50,4 +50,8 @@ public class BookInfo {
 
         return lista;
     }
+
+    public String getTitle(){
+        return ;
+    }
 }
--- app/src/main/java/com/example/googlebooksclient/BooksResultListAdapter.java
+++ app/src/main/java/com/example/googlebooksclient/BooksResultListAdapter.java
@@ -1,7 +1,10 @@
 package com.example.googlebooksclient;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,33 +15,44 @@ import java.util.List;
 public class BooksResultListAdapter extends RecyclerView.Adapter<BooksResultListAdapter.ViewHolder>{
 
     private ArrayList<BookInfo> mBooksData;
+    private LayoutInflater inflater;
 
     protected class ViewHolder extends RecyclerView.ViewHolder{
 
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        protected TextView title;
+        protected TextView author;
+        protected TextView url;
+        private BooksResultListAdapter booksResultListAdapter;
 
+        public ViewHolder(@NonNull View itemView, BooksResultListAdapter booksResultListAdapter) {
+            super(itemView);
+            title = itemView.findViewById(R.id.titleText);
+            author = itemView.findViewById(R.id.authorText);
+            url = itemView.findViewById(R.id.urlText);
+            this.booksResultListAdapter = booksResultListAdapter;
         }
     }
 
-    public BooksResultListAdapter(){
-        mBooksData = new ArrayList<>();
+    public BooksResultListAdapter(Context context, ArrayList<BookInfo> bI){
+        inflater = LayoutInflater.from(context);
+        mBooksData = bI;
     }
 
     @Override
     public BooksResultListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = this.inflater.inflate(R.layout.recyclerView, parent, false);
+        View itemView = this.inflater.inflate(R.layout.activity_main, parent, false);
         return new ViewHolder(itemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull BooksResultListAdapter.ViewHolder holder, int position) {
-
+        BookInfo mCurrent = mBooksData.get(position);
+        holder.title.setText(mCurrent.toString());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mBooksData.size();
     }
 
 
