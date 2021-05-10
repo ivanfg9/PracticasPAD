ec37ef03ce6877c8bf28f74df43d87e305a482c4
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-05-10 19:54:01 +0200

    AcercaDe, Rendimiento

---
+++ GymSy/app/src/main/java/com/example/gymsy/AcercaDe.java
@@ -0,0 +1,11 @@
+package com.example.gymsy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AcercaDe extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acerca_de);
+    }
+}
---
+++ GymSy/app/src/main/java/com/example/gymsy/Rendimiento.java
@@ -0,0 +1,6 @@
+package com.example.gymsy;
+
+import android.app.Activity;
+
+public class Rendimiento extends Activity {
+}
