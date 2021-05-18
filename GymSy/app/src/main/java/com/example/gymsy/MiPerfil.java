e222f9872ff6482d3871d4e760571a36db03d765
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-05-18 20:15:34 +0200

    MiPefil y DiaSemanaFragment, vistas de miPerfil y contenido de mi perfil

--- GymSy/app/src/main/java/com/example/gymsy/MiPerfil.java
+++ GymSy/app/src/main/java/com/example/gymsy/MiPerfil.java
@@ -1,16 +1,45 @@
 package com.example.gymsy;
 
-import android.app.Activity;
+import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MiPerfil extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MiPerfil extends AppCompatActivity {
     private ImageView imagenPerfil;
     private TextView nombreUsuario;
-    private TextView contraseña;
     private TextView altura;
     private TextView peso;
     private TextView frecAct;
 
-    
+    public MiPerfil(){
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mi_perfil);
+
+        imagenPerfil = findViewById(R.id.imageView);
+        nombreUsuario = findViewById(R.id._nombreUser);
+        altura = findViewById(R.id._alt);
+        peso = findViewById(R.id._pes);
+        frecAct = findViewById(R.id._frecActual);
+
+        DiaSemanaFragment fragment = (DiaSemanaFragment)
+                getSupportFragmentManager().findFragmentById( R.id.ejercicio_container);
+
+
+        if(fragment==null){
+            fragment = new DiaSemanaFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.ejercicio_container, fragment)
+                    .commit();
+        }
+
+    }
+
 }
